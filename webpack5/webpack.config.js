6454bbf673f06a9c4259de86fec5ac5cec13648f
gommpo <gommpo111@gmail.com>
2020-10-30 19:03:35 +0900

    multi output entry testing

--- webpack5/webpack.config.js
+++ webpack5/webpack.config.js
@@ -1,6 +1,18 @@
 const path = require('path');
 
 module.exports = {
+  entry: {
+    math: { import: './src/math.js', filename: 'pages/math/[name].js' },
+    inner: { import: './src/inner.js', filename: 'pages/inner/[name]-2.js' }
+  },
+  output: { filename: '[name].js', path: __dirname + '/dist' },
+  mode: 'production',
+  optimization:{
+    minimize: true,
+  }
+};
+/*
+module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -11,3 +23,4 @@ module.exports = {
     minimize: true,
   }
 };
+*/
