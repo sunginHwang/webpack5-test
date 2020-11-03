85a99c8ff9098eae434721fb437dc2744bd6e6f0
gommpo <gommpo111@gmail.com>
2020-11-03 22:04:57 +0900

    webpack entry 라이브러리 개별 정의

--- webpack5/webpack.config.js
+++ webpack5/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+/*// multui entry
 module.exports = {
   entry: {
     math: { import: './src/math.js', filename: 'pages/math/[name].js' },
@@ -10,7 +10,7 @@ module.exports = {
   optimization:{
     minimize: true,
   }
-};
+};*/
 /*
 module.exports = {
   entry: './src/index.js',
@@ -24,3 +24,33 @@ module.exports = {
   }
 };
 */
+
+module.exports = {
+  entry: {
+    commonjs: {
+      import: './src/math.js',
+      library: {
+        type: 'commonjs-module'
+      }
+    },
+    amd: {
+      import: './src/math.js',
+      library: {
+        type: 'amd'
+      }
+    },
+    umd: {
+      import: './src/math.js',
+      library: {
+        type: 'umd'
+      }
+    }
+  },
+  output: { filename: '[name].js', path: __dirname + '/dist' },
+  mode: 'production',
+  optimization:{
+    minimize: true,
+  }
+};
+
+
